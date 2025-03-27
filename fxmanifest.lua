da3a3f5eddc054e7bce245ab02ad485500b82d77
se9p <71358264+ise9p@users.noreply.github.com>
2025-03-27 09:32:37 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,19 +2,27 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Se9p'
-description 'This script adds NPC interactions and City Hall functionality with job selection'
+description 'City Hall system with multiple UI framework support'
 version '1.0.0'
 
 shared_script {
     'config.lua',
-    '@ox_lib/init.lua'
+    '@ox_lib/init.lua', -- [Optional]
 }
 
 client_scripts {
+    '@mt_lib/client/mt_lib.lua', -- [Optional]
     'client/*.lua',
 }
 
 server_scripts {
     'server/*.lua',
 }
+
+dependencies {
+    'qb-core',
+    'ox_lib', -- [Optional]
+    'mt_lib', -- [Optional]
+}
+
 lua54 'yes'
